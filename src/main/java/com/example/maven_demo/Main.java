87c12c47421372029dc685cf1ef3f7920fa62189
Margarita_Murazyan <murazyan.margarita@mail.ru>
2023-02-20 20:27:33 +0400

    code refactor

--- src/main/java/com/example/maven_demo/Main.java
+++ src/main/java/com/example/maven_demo/Main.java
@@ -17,46 +17,14 @@ public class Main {
         application.start();
     }
 
-   @SneakyThrows
-   public static void initDb(){
-       Properties properties = new Properties();
-       properties.load(
-//               Main.class.getClassLoader().getResourceAsStream("db\\migration\\application.properties")
-               new FileInputStream("C:\\Users\\Margarita_Murazyan\\Desktop\\maven_demo\\src\\main\\resources\\application.properties"));
-       FluentConfiguration configuration = Flyway.configure()
-               .configuration(properties);
-       Flyway flyway = new Flyway(configuration);
-       flyway.migrate();
+    @SneakyThrows
+    public static void initDb(){
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("C:\\Users\\Margarita_Murazyan\\Desktop\\maven_demo\\src\\main\\resources\\application.properties"));
+        FluentConfiguration configuration = Flyway.configure()
+                .configuration(properties);
+        Flyway flyway = new Flyway(configuration);
+        flyway.migrate();
     }
 
-//    @SneakyThrows
-//    public static void main(String[] args) {
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        Connection connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/test_db", "root", "devm");
-//        Statement statement = connection.createStatement();
-//        ResultSet resultSet = statement.executeQuery("select * from users");
-//        while (resultSet.next()){
-//            int id = resultSet.getInt("id");
-//            String name = resultSet.getString("name");
-//            String surname = resultSet.getString("surname");
-//            System.out.println(String.format("Id = %s, name = %s, surname=%s",id, name, surname ));
-//        }
-//
-//        System.out.println("********************* ");
-//        int id = 1;
-//        String name = "Poxos";
-//
-//        PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ? and name = ?");
-//        preparedStatement.setInt(1, id);
-//        preparedStatement.setString(2, name);
-//        ResultSet rs = preparedStatement.executeQuery();
-//        while (rs.next()){
-//            int id1 = rs.getInt("id");
-//            Date birthDate = rs.getDate("birth_date");
-//            System.out.println(String.format("id = %s, birthDate = %s", id1, birthDate));
-//
-//        }
-//
-//
-//    }
 }
